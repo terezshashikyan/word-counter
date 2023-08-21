94804d250e5280121471210b329edf9c4d4b01de
terezshashikyan <78693438+terezshashikyan@users.noreply.github.com>
2023-08-22 01:21:50 +0400

    final changes

---
+++ script/index.js
@@ -0,0 +1,39 @@
+ let sentences = document.getElementById("sentences").getElementsByTagName('h3')[0];
+ let words = document.getElementById("words").getElementsByTagName('h3')[0];
+ let characters = document.getElementById("characters").getElementsByTagName('h3')[0];
+ 
+
+ function countSentences() {
+    let text = document.getElementById("text").value;
+    const re = /[.!?]/;
+    const numOfSentences = text.split(re);
+    sentences.innerHTML = numOfSentences.length - 1;
+  };
+
+  function countWords() {
+     let text = document.getElementById("text").value;
+     const arr = text.split(' ');
+     let countOfWords =  arr.filter(word => word !== '').length;
+     words.innerHTML = countOfWords;
+    
+  };
+
+  function countCharacters() {
+    let text = document.getElementById("text").value;
+    // use / /g to remove all spaces from the string
+    let countOfCharacters = text.trim().replace(/ /g, "").length;
+    characters.innerHTML = countOfCharacters;
+
+  };
+
+ function getRandomColor() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+  
+    document.body.style.background = bgColor;
+    }
+
+   
+ 
